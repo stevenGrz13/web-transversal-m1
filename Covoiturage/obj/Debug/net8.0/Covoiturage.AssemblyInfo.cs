fc541ae659d035da67e3af9c482bd2f97bcf8886
stevenGrz13 <razafimahefasteven130102@gmail.com>
2025-09-16 10:48:25 +0300

    changements

--- Covoiturage/obj/Debug/net8.0/Covoiturage.AssemblyInfo.cs
+++ Covoiturage/obj/Debug/net8.0/Covoiturage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Covoiturage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30971b71d7d5a1a66ba7af36bd27b542f287eec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328857a782f34cc673c5b3b10c985b2ba1063c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Covoiturage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Covoiturage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
